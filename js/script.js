f5ae08515b6d3609847807018f63fd5c245636a2
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-01-21 23:35:51 +0100

    prep

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-*/**Descrizione:**
+/**Descrizione:**
 Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
 Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
@@ -15,5 +15,4 @@ Consigli del giorno:
 > - Individuate gli elementi di cui avete bisogno per realizzare il programma.
 > - Immaginate la logica come fosse uno snack: "Dati 2 array di numeri, indica quali e quanti numeri ci sono in comune tra i due array"*/
 
-let numeriRandom = function random()
-let
+
